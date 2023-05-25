13e062cf2e570f1fc0d85374cc23fdb614ee0f49
Alexander Thaler <alexander.karl.thaler@gmail.com>
2023-05-25 17:29:42 +0200

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists do
+    resources :bookmarks, only: %i[index new create]
+  end
+  resources :bookmarks, only: %i[show edit update destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+Movie.destroy_all
+puts 'Creating 100 fake movies...'
+100.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: "https://source.unsplash.com/random/1000%C3%97500/?movie",
+    rating: rand(1.0..10.0).round(1)
+  )
+  puts movie.save
+end
